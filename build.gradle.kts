fbd4692f7d8bac0af4b5c4398cfd51a115142193
Natthaphon Phanit <kekhuay@users.noreply.github.com>
2020-12-11 23:25:16 +0700

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    application
+    idea
+    eclipse
+    kotlin("jvm") version "1.4.10"
+    java
+}
+
+group = "com.nexterditigals.nextershop"
+version = "0.0.1"
+java {
+    sourceCompatibility = JavaVersion.VERSION_14
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.protobuf:protobuf-java:3.14.0")
+    implementation("io.grpc:grpc-kotlin-stub:0.2.1")
+    implementation("io.grpc:grpc-stub:1.34.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+sourceSets["main"].java {
+    srcDir("src/main/grpc")
+    srcDir("src/main/grpckt")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "14"
+    }
+}
