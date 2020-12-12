3695d67447634cb37752f774d08ade43406007e2
Natthaphon Phanit <kekhuay@users.noreply.github.com>
2020-12-12 16:39:24 +0700

    store version number in variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+val version: String by project
+val projectVersion = version
+val usr: String by project
+val pwd: String by project
+val protobufJavaVersion = "3.14.0"
+val grpcKotlinStubVersion = "0.2.1"
+val grpcStubVersion = "1.34.0"
+val javaxAnnotationApiVersion = "1.3.2"
+
 plugins {
     application
     idea
@@ -19,10 +28,10 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.google.protobuf:protobuf-java:3.14.0")
-    implementation("io.grpc:grpc-kotlin-stub:0.2.1")
-    implementation("io.grpc:grpc-stub:1.34.0")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("com.google.protobuf:protobuf-java:$protobufJavaVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinStubVersion")
+    implementation("io.grpc:grpc-stub:$grpcStubVersion")
+    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationApiVersion")
 }
 
 sourceSets["main"].java {
@@ -36,11 +45,6 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         jvmTarget = "14"
     }
 }
-
-val version: String by project
-val projectVersion = version
-val usr: String by project
-val pwd: String by project
 
 publishing {
     publications {
